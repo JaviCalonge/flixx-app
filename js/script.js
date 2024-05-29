b105d5eda4d80a61ad1957e584737daf11d71887
Javi Calonge <javicalongem@gmail.com>
2024-05-29 17:16:17 +0200

    Set fetchAPIData function

--- js/script.js
+++ js/script.js
@@ -2,6 +2,20 @@ const global = {
   currentPage: window.location.pathname
 }
 
+//Fetch data from TMDB API
+async function fetchApiData () {
+  const API_KEY = "31079babc1cb1333fc04de99dd3e5658"
+  const API_URL = "https://api.themoviedb.org/3/"
+
+  const response = await fetch(`${API_URL}${endpoint}?api_key=${API_KEY}&language=es-ES`)
+
+  const data = await response.json()
+
+  return data
+}
+
+
+
 //Highlight active link
 function highligthActiveLink () {
   const links = document.querySelectorAll(".nav-link")
