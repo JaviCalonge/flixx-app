5bc7b85c25ff67c89be2deeccc421b161e5f6e86
Javi Calonge <javicalongem@gmail.com>
2024-05-30 12:59:02 +0200

    Add async function displayShowDetails

--- js/script.js
+++ js/script.js
@@ -137,6 +137,73 @@ async function displayMovieDetails () {
   document.querySelector("#movie-details").appendChild(div)
 }
 
+//Display Show Details
+async function displayShowDetails () {
+  
+  const tvId = window.location.search.split("=")[1]
+
+  const tv = await fetchAPIData(`tv/${tvId}`)
+
+
+//Overlay for background image
+
+  displayBackgroundImage("tv", tv.backdrop_path)
+
+  const div = document.createElement("div")
+  div.innerHTML = `
+  <div class="details-top">
+  <div>
+  ${
+    tv.poster_path
+    ? `<img
+    src="https://image.tmdb.org/t/p/w500${tv.poster_path}"
+    class="card-img-top"
+    alt="&{movie.name}"
+  />` : `<img
+  src="images/no-image.jpg"
+  class="card-img-top"
+  alt="&{movie.name}"
+/>`
+  }
+  </div>
+  <div>
+    <h2>${tv.name}</h2>
+    <p>
+      <i class="fas fa-star text-primary"></i>
+      ${tv.vote_average.toFixed(1)} / 10
+    </p>
+    <p class="text-muted">Last Air Date: ${tv.last_air_date}</p>
+    <p>
+      ${tv.overview}
+    </p>
+    <h5>Genres</h5>
+    <ul class="list-group">
+      ${tv.genres.map((genre) => `<li>${genre.name}</li>`).join("")}
+      
+    
+    </ul>
+    <a href= ${tv.homepage} target="_blank" class="btn">Visit Show Homepage</a>
+  </div>
+</div>
+<div class="details-bottom">
+  <h2>Show Info</h2>
+  <ul>
+    <li><span class="text-secondary">Number Of Episodes:</span> ${tv.number_of_episodes}</li>
+    <li><span class="text-secondary">Last Episode To Air</span> ${tv.last_episode_to_air.name}</li>
+    <li><span class="text-secondary">Status:</span> ${tv.status}</li>
+  </ul>
+  <h4>Production Companies</h4>
+  <div class="list-group">
+  ${tv.production_companies.map((productor) => `${productor.name}`).join(", ")}
+  </div>
+</div>
+  `
+  document.querySelector("#show-details").appendChild(div)
+}
+
+/////////////////////////////////////////////////////////////////////////
+
+
 //Display Backdrop on details pages
 
 function displayBackgroundImage (type, backgroundPath) {
@@ -217,7 +284,7 @@ function init () {
       displayMovieDetails()
       break 
      case "/tv-details.html":
-      console.log("TV details")
+      displayShowDetails()
       break
     case "/search.html":
       console.log("Search")
