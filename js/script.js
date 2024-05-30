a253d8ad5355d57569f5aef9c77de11cf22ff507
Javi Calonge <javicalongem@gmail.com>
2024-05-30 19:16:44 +0200

    Display search results

--- js/script.js
+++ js/script.js
@@ -26,11 +26,11 @@ async function displayPopularMovies () {
               ? `<img
               src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
               class="card-img-top"
-              alt="&{movie.title}"
+              alt="${movie.title}"
             />` : `<img
             src="images/no-image.jpg"
             class="card-img-top"
-            alt="&{movie.title}"
+            alt="${movie.title}"
           />`
             }
           </a>
@@ -104,7 +104,8 @@ async function displayMovieDetails () {
     src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
     class="card-img-top"
     alt="&{movie.title}"
-  />` : `<img
+  />` 
+  : `<img
   src="images/no-image.jpg"
   class="card-img-top"
   alt="&{movie.title}"
@@ -244,12 +245,56 @@ async function search () {
   global.search.type = urlParams.get("type")
 
   if (global.search.term !== "" && global.search.term !== null) {
-    const results = await searchAPIData()
-    console.log(results)
+    const { results, total_pages, page } = await searchAPIData()
+    
+    if (results.length === 0) {
+      showAlert("No results found", "error")
+      return
+    }
+
+    displaySearchResults(results)
+
+    document.querySelector("#search-term").value = ""
+
   } else {
   showAlert("Please, enter a search term")
   }
 }
+
+//Display search results
+function displaySearchResults (results) {
+  results.forEach((result) => {
+    const div = document.createElement("div")
+    div.classList.add("card")
+    div.innerHTML = `
+          <a href="${global.search.type}-details.html?id=${result.id}">
+            ${
+              result.poster_path
+              ? `<img
+              src="https://image.tmdb.org/t/p/w500${result.poster_path}"
+              class="card-img-top"
+              alt="${global.search.type === "movie" ? result.title : result.name}"
+            />` 
+            : `<img
+            src="images/no-image.jpg"
+            class="card-img-top"
+            alt="${global.search.type === "movie" ? result.title : result.name}"
+          />`
+            }
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${global.search.type === "movie" ? result.title : result.name}</h5>
+            <p class="card-text">
+              <small class="text-muted">Release: ${global.search.type === "movie" ? result.release_date 
+              : result.first_air_date}</small>
+            </p>
+          </div>
+        </div>
+    `
+    document.querySelector("#search-results").appendChild(div)
+  })
+}
+
 
 //Display Slider movies
 async function displaySlider () {
